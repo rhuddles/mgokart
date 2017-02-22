5995d81764ff98dee97b846f63280324f28ab705
Cooper Young <coopery@umich.edu>
2017-02-22 15:16:47 -0500

    Modify curvature_steering plots to match others.

--- software/curvature_steering.py
+++ software/curvature_steering.py
@@ -8,15 +8,21 @@ import filter_data as fd
 import greedy as g
 import os
 
-files = sorted(os.listdir('data'))
-
-filename = 'data/' + files[5]
-print(filename)
+filename = 'data/2017_02_19_19_51_09_729_sharp_right.csv'
 frames = pd.parse_csv_data(filename)
 cones = fd.get_cones(frames[0])
-plt.scatter([pt[0] for pt in cones], [pt[1] for pt in cones], color='k')
+
+plt.scatter([pt[0] for pt in cones], [pt[1] for pt in cones], color='b', marker='^')
 
 boundaries = g.create_boundary_lines(cones)
+
+left_xs = [point[0] for point in boundaries[0]]
+left_ys = [point[1] for point in boundaries[0]]
+plt.plot(left_xs, left_ys, color='orange')
+
+right_xs = [point[0] for point in boundaries[1]]
+right_ys = [point[1] for point in boundaries[1]]
+plt.plot(right_xs, right_ys, color='red')
 
 x_plot = np.linspace(-10000, 10000, 2)
 
@@ -26,9 +32,11 @@ vecy = np.mean([boundaries[0][len(boundaries[0])-1][1], boundaries[1][len(bounda
 print m.degrees(m.atan(vecx/vecy))
 nl = np.polyfit([0, vecx], [0, vecy], 1)
 f = np.poly1d(nl)
-plt.plot(x_plot, f(x_plot), 'r')
 
-plt.xlim([-10000, 10000])
-plt.ylim([-10000, 10000])
+plt.plot(x_plot, f(x_plot), 'k')
+plt.scatter(0, 0, color='g')
+
+plt.xlim([-6000, 8000])
+plt.ylim([-4000, 8000])
 plt.show()
 
