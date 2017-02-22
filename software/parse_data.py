0b39948f8335c07e084cc4963b5e94fbd9bac383
Cooper Young <coopery@umich.edu>
2017-02-22 15:14:02 -0500

    Make parse_data plot all files.

--- software/parse_data.py
+++ software/parse_data.py
@@ -2,6 +2,8 @@
 
 import csv
 import math
+import matplotlib.pyplot as plt
+import os
 
 def polar_to_cart(theta, dist):
     rads = math.radians(theta)
@@ -32,5 +34,12 @@ def parse_csv_data(filename):
     return frames
 
 if __name__ == '__main__':
-    frames = parse_csv_data('data/wilson_center_wall.csv')
-    print(frames)
+    files = sorted(os.listdir('data'))
+    for filename in files:
+        frames = parse_csv_data('data/' + filename)
+        frame = frames[0]
+        x = [pt[0] for pt in frame]
+        y = [pt[1] for pt in frame]
+        plt.scatter(x, y, color='b', marker='.')
+        plt.scatter(0, 0, color='g')
+        plt.show()
