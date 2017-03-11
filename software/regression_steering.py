4a36961d12abcf8da7e0741e46dd433e3ed8729c
Cooper Young <coopery@umich.edu>
2017-03-11 17:08:43 -0500

    Add point choosing along center path.

--- software/regression_steering.py
+++ software/regression_steering.py
@@ -13,58 +13,85 @@ def plot_line(coefs, min_x, max_x, style='-k'):
     x = np.linspace(min_x, max_x, 20000)
     return plt.plot(x, f(x), style)
 
-filename = 'data/grass_sharp_right.csv'
-frame = parse_csv_data(filename)[0]
-cones = get_cones(frame)
-
-# Plot cones
-cone_xs, cone_ys = separate_xy(cones)
-blue = plt.scatter(cone_xs, cone_ys, color='blue', marker='^')
-
-left_boundary, right_boundary = create_boundary_lines(cones)
-
-# Plot left boundary
-left_xs, left_ys = separate_xy(left_boundary)
-orange, = plt.plot(left_xs, left_ys, color='orange')
-
-# Plot right boundary
-right_xs, right_ys = separate_xy(right_boundary)
-red, = plt.plot(right_xs, right_ys, color='red')
-
-# Fit left line
-left_coefs = np.polyfit(left_xs, left_ys, 2)
-dashed, = plot_line(left_coefs, min(left_xs), max(left_xs), '--k')
-
-# Fit right line
-right_coefs = np.polyfit(right_xs, right_ys, 2)
-plot_line(right_coefs, min(right_xs), max(right_xs), '--k')
-
-# Calculate center line
-avg_coefs = np.add(left_coefs, right_coefs) / 2
-black, = plot_line(avg_coefs, min(left_xs + right_xs), max(left_xs + right_xs))
-
 def predict_next_pos(speed, heading, dt):
     vel = np.array([math.cos(heading), math.sin(heading)])
     vel *= speed
 
     return vel * dt
 
-next_pos = predict_next_pos(1000, 0, 0.3)
+def get_next_steering_angle(path_coefs, next_pos):
+    path_xs = np.linspace(-5000, 5000, 10000)
+    path = np.poly1d(path_coefs)
 
-green = plt.scatter(0, 0, color='green')
-cyan = plt.scatter(next_pos[0], next_pos[1], color='cyan')
+    path_pts = [(x, path(x)) for x in path_xs]
 
-# Make plot look nice for report
-plt.xlabel('Distance in millimeters')
-plt.ylabel('Distance in millimeters')
+    chosen_pt = min(path_pts, key=lambda pt: dist(next_pos, pt))
+    plt.scatter(chosen_pt[0], chosen_pt[1], color='red', marker='x')
 
-plt.legend(
-    (orange, red, dashed, black, blue, green),
-    ('Left Boundary', 'Right Boundary', 'Regression Line', 'Trend Line', 'Detected Cone', 'Vehicle Position'),
-    loc='upper left'
-)
+    return angle_between((1, 0), chosen_pt)
 
-plt.axis('equal')
-plt.axis([-6000, 10000, -5000, 10000])
-plt.title('Regression Steering')
-plt.show()
+def flip_xy(x, y):
+    return (y, [-a for a in x])
+
+def separate_and_flip(points):
+    return flip_xy(*separate_xy(points))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        files = sys.argv[1:]
+    else:
+        files = ['data/' + path for path in sorted(os.listdir('data'))]
+
+    for filename in files:
+        frame = parse_csv_data(filename)[0]
+
+        cones = get_cones(frame)
+        cone_xs, cone_ys = separate_and_flip(cones)
+
+        # Separate into boundaries
+        left_boundary, right_boundary = create_boundary_lines(cones)
+
+        # Fit left line
+        left_xs, left_ys = separate_and_flip(left_boundary)
+        left_coefs = np.polyfit(left_xs, left_ys, 2)
+
+        # Fit right line
+        right_xs, right_ys = separate_and_flip(right_boundary)
+        right_coefs = np.polyfit(right_xs, right_ys, 2)
+
+        # Calculate center line
+        avg_coefs = np.add(left_coefs, right_coefs) / 2
+
+        # make a decision for once in your life gavin
+        next_pos = predict_next_pos(1000, 0, 0.3)
+        angle = get_next_steering_angle(avg_coefs, next_pos)
+        print('Gotta turn %f degrees in some direction' % math.degrees(angle))
+
+        # plot just everything
+        print(filename)
+        blue = plt.scatter(cone_xs, cone_ys, color='blue', marker='^')
+        orange, = plt.plot(left_xs, left_ys, color='orange')
+        red, = plt.plot(right_xs, right_ys, color='red')
+
+        dashed, = plot_line(left_coefs, min(left_xs), max(left_xs), '--k')
+        plot_line(right_coefs, min(right_xs), max(right_xs), '--k')
+        black, = plot_line(avg_coefs, min(left_xs + right_xs),
+                max(left_xs + right_xs))
+
+        green = plt.scatter(0, 0, color='green')
+        cyan = plt.scatter(next_pos[0], next_pos[1], color='cyan')
+
+        # Make plot look nice for report
+        plt.xlabel('Distance in millimeters')
+        plt.ylabel('Distance in millimeters')
+
+        plt.legend(
+            (orange, red, dashed, black, blue, green),
+            ('Left Boundary', 'Right Boundary', 'Regression Line', 'Trend Line', 'Detected Cone', 'Vehicle Position'),
+            loc='upper left'
+        )
+
+        plt.axis('equal')
+        plt.axis([-6000, 10000, -5000, 10000])
+        plt.title('Regression Steering')
+        plt.show()
