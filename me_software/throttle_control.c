6ace5886822fea726837b34a2187a703e0fdb6b4
Edison <root@edison.com>
2017-03-30 04:55:36 +0000

    Make throttle_control usable for ME's demo

--- me_software/throttle_control.c
+++ me_software/throttle_control.c
@@ -1,6 +1,8 @@
 #include <mraa/aio.h>
 #include <mraa/pwm.h>
 
+#include <signal.h>
+
 const int THROTTLE_SIGNAL_ANA = 0;
 const int THROTTLE_SIGNAL_PWM = 5;
 
@@ -8,6 +10,10 @@ const int MANUAL_SWITCH_PIN = 9;
 
 const int REFERENCE_VOLTAGE = 5;
 const int BATTERY_VOLTAGE = 9;
+
+int running = 1;
+
+void stop_running(int signal);
 
 // If manual we read from accelerator's analog signal
 // If not we read from electrical team's m/s command
@@ -17,8 +23,8 @@ float read_mps();
 
 int main(void)
 {
-    // Init GPIO
-    mraa_gpio_context manual_switch = mraa_gpio_init(MANUAL_SWITCH_PIN);
+	// Init GPIO
+	mraa_gpio_context manual_switch = mraa_gpio_init(MANUAL_SWITCH_PIN);
 	mraa_gpio_dir(manual_switch, MRAA_GPIO_IN);
 
 	// Init Analog
@@ -28,23 +34,26 @@ int main(void)
 	mraa_pwm_context throttle_out = mraa_pwm_init(THROTTLE_SIGNAL_PWM);
 	mraa_pwm_period_us(throttle_out, 50);	
 	mraa_pwm_enable(throttle_out, 1);
-	
-	while (1) {
-        // Check if manual or autonomous
-        int manual = mraa_gpio_read(manual_switch);
-        
-        float signal_out;
+
+	signal(SIGINT, stop_running);                                               
+	signal(SIGABRT, stop_running);
+
+	while (running) {
+		// Check if manual or autonomous
+		int manual = mraa_gpio_read(manual_switch);
+
+		float signal_out;
 
 		// Read Analog
-        if (manual) {
-            fprintf(stderr, "Manual Mode\n");
-            signal_out = read_analog_signal(throttle_in);
-        }
-        // Read m/s
-        else {
-            fprintf(stderr, "Autonomous Mode\n");
-            signal_out = read_mps();
-        }
+		if (manual) {
+		    fprintf(stderr, "Manual Mode\n");
+		    signal_out = read_analog_signal(throttle_in);
+		}
+		// Read m/s
+		else {
+		    fprintf(stderr, "Autonomous Mode\n");
+		    signal_out = read_mps();
+		}
 
 		// Write PWM
 		mraa_pwm_write(throttle_out, signal_out);
@@ -53,26 +62,34 @@ int main(void)
 		usleep(1000000); // 1s
 	}
 
-    mraa_gpio_close(manual_switch);
+	mraa_gpio_close(manual_switch);
 
 	mraa_aio_close(throttle_in);
 
+	fprintf(stderr, "\nClosing...\n");
+	fprintf(stderr, "Write 0 to pin %d\n", THROTTLE_SIGNAL_PWM);
 	mraa_pwm_write(throttle_out, 0);
 	mraa_pwm_close(throttle_out);
 
-    return 0;
+	return 0;
+}
+
+void stop_running(int signal)
+{
+	running = 0;
 }
 
 float read_analog_signal(mraa_aio_context throttle)
 {
-    float throttle_signal = mraa_aio_read_float(throttle);
-    fprintf(stderr, "Read %f from pin %d\n", throttle_signal, THROTTLE_SIGNAL_ANA);
+	float throttle_signal = mraa_aio_read_float(throttle);
+	fprintf(stderr, "Read %f from pin %d\n", throttle_signal, THROTTLE_SIGNAL_ANA);
 
-    return throttle_signal * REFERENCE_VOLTAGE / BATTERY_VOLTAGE;
+	return throttle_signal * REFERENCE_VOLTAGE / BATTERY_VOLTAGE;
 }
 
 float read_mps()
 {
-    float mps = .45; // 1 mph
-    return (mps / MAX_MPS) * BATTERY_VOLTAGE
+	// Based on 9V reference
+	// Want about 3V
+	return 0.33;
 }
